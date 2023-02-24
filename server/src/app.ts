6e29ddd53bf65c58401c9fd6e04cddbe9cba717d
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-24 22:20:23 +0530

    configured mongo db connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import questionRoutes from "./api/routes/question.js";
 
 import dotenv from "dotenv";
-
+import db from "./api/utils/db.js"
 
 const app = express();
+db.on("error", console.error.bind(console, "connection error:"));
 
 dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
 
