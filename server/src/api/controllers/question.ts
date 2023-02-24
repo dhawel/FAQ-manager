f3efa1a65e8037a52a089f149b401c951db8fcd1
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-24 22:56:01 +0530

    Add getQuestions API endpoint

--- server/src/api/controllers/question.ts
+++ server/src/api/controllers/question.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 
-
+import Question  from "../models/question.js";
 export const getQuestions = async (req: Request, res: Response) => {
   try {
+    const questions = await Question.find();
 
-    res.status(200).json("question route !!!");
-  } catch (err) {
-   
+    res.status(200).json(questions);
+  } catch (err:any) {
+    res.status(500).json({ message: err.message });
   }
 };
 
