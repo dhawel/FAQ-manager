6439dac99e9f0b4e6fffad24fd4f937f364a1488
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-24 23:03:32 +0530

    Add getQuestionById and createQuestion handlers

--- server/src/api/controllers/question.ts
+++ server/src/api/controllers/question.ts
@@ -1,14 +1,41 @@
 import { Request, Response } from "express";
 
-import Question  from "../models/question.js";
+import Question ,{IQuestion} from "../models/question.js";
 export const getQuestions = async (req: Request, res: Response) => {
   try {
     const questions = await Question.find();
 
     res.status(200).json(questions);
-  } catch (err:any) {
-    res.status(500).json({ message: err.message });
+  } catch (err) {
+    res.status(500).json({ message: err});
   }
 };
+
+
+export const getQuestionById = async (req: Request, res: Response) => {
+    try {
+      const question = await Question.findById(req.params.id);
+      if (!question) {
+        res.status(404).json({ message: "Question not found" });
+        return;
+      }
+      res.status(200).json(question);
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  };
+
+  export const createQuestion = async (req: Request, res: Response) => {
+    try {
+      const { question, catagory } = req.body;
+      const newQuestion: IQuestion = new Question({ question, catagory });
+      await newQuestion.save();
+      res.status(200).json(question);
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  };
+
+
 
 
