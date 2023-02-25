c95db9f80cdf0016fe09db04ebbb96188ea64a70
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-25 23:22:18 +0530

    created basic page structure

--- client/pages/faq/index.tsx
+++ client/pages/faq/index.tsx
@@ -1,11 +1,30 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import SearchButton from "@/components/SearchButton";
-import SearchBox from "@/components/SearchBox";
+// import SearchBox from "@/components/SearchBox";
+import FaqTable from "@/components/FaqTable"
+import styles from '@/styles/Myfaq.module.css'
 const Faq = () => {
   return (
     <>
-    <SearchBox/>
-    <SearchButton/>
+
+    {/* <SearchButton/> */}
+    <div className={styles.container}>
+     <div className={styles.header} >
+        <h1  className={styles.tittle} >FAQ Manger - iLabs </h1>
+        <div className={styles.modal}>Add New question</div>
+     </div>
+     <div className={styles.search} >
+        <div className={styles.searchbox}></div>
+        <div className={styles.searchbutton}></div>
+     </div>
+     <div className={styles.table} >
+        <FaqTable/>
+     </div>
+     <div className={styles.footer} >
+       footer
+     </div>
+    </div>
+
     </>
   )
 };
