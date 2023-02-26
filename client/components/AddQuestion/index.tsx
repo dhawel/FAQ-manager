f28438753c5ced9a89633aa8f7c4a776c9823f80
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-26 12:43:26 +0530

    implement input fields question and category selector

--- client/components/AddQuestion/index.tsx
+++ client/components/AddQuestion/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import AddQuestionButton from "./AddQuestionButton";
+import MenuItem from "@mui/material/MenuItem";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { Input } from "@mui/material";
+import TextField from "@mui/material/TextField";
+
 const style = {
   position: "absolute" as "absolute",
   top: "50%",
@@ -21,24 +29,79 @@ const AddQuestion = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  let [Question, setQuestion] = useState("");
+  let [Category, setCategory] = useState("");
+  const Catagories = [
+    {
+      value: "About Company",
+      label: "About Company",
+    },
+    {
+      value: "Account",
+      label: "Account",
+    },
+    {
+      value: "General",
+      label: "General",
+    },
+    {
+      value: "Product",
+      label: "Product",
+    },
+  ];
+  const SubmitBtn = () => {
+    console.log("sumbittt",Question,Category);
+  };
+
   return (
     <>
       <AddQuestionButton clickHandler={handleOpen}>
-        Add New Question
+        <AddCircleIcon color="primary" aria-label="add new question" /> Add New
+        Question
       </AddQuestionButton>
+      {/* <Button variant="contained" startIcon={<AddCircleIcon />}>
+        Add New Question
+      </Button> */}
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="add new question box"
+        aria-describedby="add new faq question"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+         
+          <TextField
+            fullWidth
+            label="Question"
+            id="fullWidth"
+            className="input"
+            type="text"
+            required
+            value={Question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <br />
+          <br />
+          <TextField
+            id="fullWidth"
+            select
+            label="Category"
+            defaultValue="General"
+            helperText="Please select the category"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {Catagories.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <br />
+          <br />
+          <Button onClick={SubmitBtn} variant="contained" color="secondary">
+            Add Question
+          </Button>
         </Box>
       </Modal>
     </>
