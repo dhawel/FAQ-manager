6bd511f21a32392505a5cf9f47e973d54f373720
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-26 21:15:26 +0530

    refactor: change api endpoint name

--- client/redux/slices/faqAPI.ts
+++ client/redux/slices/faqAPI.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://faq-manager-production.up.railway.app'; // Replace with your API server URL
+const BASE_URL = "https://faq-manager-production.up.railway.app"; // Replace with your API server URL
 
 const faqApi = {
   getFaqs: async () => {
-    const response = await axios.get(`${BASE_URL}/questions`);
+    const response = await axios.get(`${BASE_URL}/faqs`);
     return response.data;
   },
   postFaq: async (newFaq) => {
-    console.log("post hit",newFaq);
-    const response = await axios.post(`${BASE_URL}/questions`, newFaq);
+    console.log("post hit", newFaq);
+    const response = await axios.post(`${BASE_URL}/faqs`, newFaq);
     return response.data;
   },
   updateFaq: async (faq) => {
-    const response = await axios.put(`${BASE_URL}/questions/${faq._id}`, faq);
+    const response = await axios.put(`${BASE_URL}/faqs/${faq._id}`, faq);
     return response.data;
   },
   deleteFaq: async (faqId) => {
-    const response = await axios.delete(`${BASE_URL}/questions/${faqId}`);
+    const response = await axios.delete(`${BASE_URL}/faqs/${faqId}`);
     return response.data;
   },
 };
