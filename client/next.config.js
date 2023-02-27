9883fabb759d51ba9dbfe9a2d369caad659c9a9c
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-27 14:57:56 +0530

    chore: reconfigure env varibales

--- client/next.config.js
+++ client/next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 }
 
 module.exports = nextConfig
