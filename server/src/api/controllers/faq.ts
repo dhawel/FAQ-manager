e1b9d9fbd2604e54a2e81c442f8342bf644407ef
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-27 15:08:14 +0530

    refactor: send only the _id

--- server/src/api/controllers/faq.ts
+++ server/src/api/controllers/faq.ts
@@ -64,7 +64,7 @@ export const deleteFaqById = async (req: Request, res: Response) => {
       res.status(404).json({ message: "Faq not found" });
       return;
     }
-    res.status(200).json(faq);
+    res.status(200).json(faq._id);
   } catch (err) {
     res.status(500).json({ message: err });
   }
