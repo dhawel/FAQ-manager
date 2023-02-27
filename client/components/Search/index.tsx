4041db43b9de3fe35f1b572458778a24e665a2d0
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-02-27 16:30:46 +0530

    feat: Add state management to search input

--- client/components/Search/index.tsx
+++ client/components/Search/index.tsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import styles from "./index.module.css";
 import Button from "@mui/material/Button";
 
+const Search = () => {
+  const [searchValue, setSearchValue] = useState(""); 
 
-const Search = ()=> {
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
 
-
+  const handleSearchClick = () => {
+    console.log("Search value:", searchValue);
+  };
   return (
     <>
-    <div className={styles.searchbox}>
-
-    <input type="text" placeholder="Search.." name="search"></input>
-
-  </div>
-  <div className={styles.searchbutton}>
-    <Button
-      fullWidth
-      sx={{
-        fontSize: 12,
-        height: 50,
-        backgroundColor: "rgb(63, 81, 181) ",
-      }}
-      variant="contained"
-      size="medium"
-    >
-      Search
-    </Button>
-  </div>
-  </>
+      <div className={styles.searchbox}>
+        <input
+          type="text"
+          placeholder="Search.."
+          name="search"
+          value={searchValue}
+          onChange={handleSearchChange}
+        ></input>
+      </div>
+      <div className={styles.searchbutton}>
+        <Button
+          fullWidth
+          sx={{
+            fontSize: 12,
+            height: 50,
+            backgroundColor: "rgb(63, 81, 181) ",
+          }}
+          variant="contained"
+          size="medium"
+          onClick={handleSearchClick}
+        >
+          Search
+        </Button>
+      </div>
+    </>
   );
 };
-
 
 export default Search;
