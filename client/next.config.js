1cf2aa1dc1bc664c3e3dfdc578543ec1fde6b8b9
Dhammika <78781596+dhawel@users.noreply.github.com>
2023-03-01 13:16:30 +0530

    refactor : remove unused

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+
+  //   ignoreDuringBuilds: true,
+  // },
   env: {
     BASE_URL: process.env.BASE_URL,
   }
